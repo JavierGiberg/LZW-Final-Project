d96e8cd72ecf3a3f8e80bb49e64ea3aace493e68
https://github.com/JavierGiberg <gibergjavier@gmail.com>
2022-07-28 17:20:38 +0300

    280722

--- src/IO.java
+++ src/IO.java
@@ -1,8 +1,43 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class IO {
 
 	public static void main(String[] args) {
-		String str = "banabananamanaman";
+		String str = "";
+		String src = "new Smiley.bmp";
+		try {
+			FileInputStream fileinputstream = new FileInputStream(src);
+			byte[] fileBytes = new byte[fileinputstream.available()];
+			fileinputstream.read(fileBytes);
+			
+			OutputStream outputstream = new FileOutputStream("ResultLZW.bin");
+			ObjectOutputStream objectoutputstream = new ObjectOutputStream(outputstream);
+			
+		
+			objectoutputstream.close();
+			outputstream.close();
+			fileinputstream.close();
+			for(int x=0;x<fileBytes.length;x++) {
+				
+				str+=" "+fileBytes[x];
+			System.out.println(str);
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		
 		// banabananamanaman
 //		System.out.println(str.length()*3);
 		LZW A = new LZW(str);
--- src/LZW.java
+++ src/LZW.java
@@ -1,9 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LZW {
+	static FileOutputStream fileOutputStream; 
+	static ObjectOutputStream objectoutputstream;
+	static FileOutputStream fileOutputStream1; 
+	static ObjectOutputStream objectoutputstream1;
 	static int index = 128;
 	static String str = "";
 	static String current = "";
@@ -18,13 +27,37 @@ public class LZW {
 	}
 
 	public void Compress() {
+		
+		try {
+			fileOutputStream = new FileOutputStream("Compressed file.bin");
+			objectoutputstream = new ObjectOutputStream(fileOutputStream);
+			fileOutputStream1 = new FileOutputStream("regular.bin");
+			objectoutputstream1 = new ObjectOutputStream(fileOutputStream1);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			objectoutputstream1.writeChars(str);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		CompressStr();
-		String result =convertStringToBinary(out);
-		//System.out.println(result);
-		System.out.println(prettyBinary(result, 8, " "));
+//		String result =convertStringToBinary(out);
+//		byte[] w = result.getBytes();
+//		System.out.println(result);
+//		System.out.println(prettyBinary(result, 8, " "));
 		
-		
-
+		try {
+		//	objectoutputstream.writeBytes(str);
+			objectoutputstream.close();
+			objectoutputstream1.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 //-----------------------------Compress str-----------------------------------------------	
@@ -53,6 +86,7 @@ public class LZW {
 
 //---------------------------check The Best-----------------------------------------------	
 	static void checkBestonDic(String toCheck, int nextChar) {
+	
 		if (str.length() == nextChar) {
 			out += toCheck + "";
 			dic.put(toCheck, index);
@@ -61,29 +95,42 @@ public class LZW {
 			System.out.println(dic);
 			return;
 		}
+	
 		if (!dic.containsKey(toCheck + "" + str.charAt(nextChar))) {
-//			System.out.println(toCheck+""+str.charAt(nextChar));
+
 			if (toCheck.length() == 1) {
 				int ascii = toCheck.charAt(0);
 				if (ascii < 128) {
-//				System.out.println("the ascii "+ascii);
+
 					out += ascii + "";
 					dic.put(ascii + "" + str.charAt(nextChar), index);
 					index++;
 					str = str.substring(nextChar);
-//				System.out.println(out);
-//				System.out.println(dic);
+					try {
+						objectoutputstream.writeByte(ascii);//write byte
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				System.out.println(out);
+				System.out.println(dic);
 					return;
 				}
 			}
-//			System.out.println("int "+nextChar);
+			System.out.println("int "+nextChar);
 //			System.out.println(str);
-			out += toCheck + "";
+			out += toCheck + " ";
 			dic.put(toCheck + str.charAt(nextChar), index);
 			index++;
 			str = str.substring(nextChar);
-//			System.out.println(out);
-//			System.out.println(dic);
+			try {
+				objectoutputstream.writeChars(toCheck);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}//write byte
+			System.out.println(out);
+			System.out.println(dic);
 			return;
 		} else {
 
